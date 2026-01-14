3e00c0a75f7c0bc4f05032e1c3bf47739c730857
Itallo Arthur <itallo2002@hotmail.com>
2026-01-14 10:49:21 -0300

    Condições no Js

---
+++ Aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 60.5
+console.log(`a velocidade do seu carro é ${vel} Km/h`)
+if(vel > 60){ //revendo condição simples no Js
+    console.log(`você ultrapassou a velocidade permitida. MULTADO!`)
+}
+console.log(`dirija sempre usando cinto de segurança`)
---
+++ Aula11/ex009.js
@@ -0,0 +1,6 @@
+var país = 'brasil'
+if(país == 'brasil'){//revendo condição no Js
+    console.log(`você é brasileiro`)
+}else{
+    console.log(`você é estrangiero`)
+}
