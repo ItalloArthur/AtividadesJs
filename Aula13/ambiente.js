7d0f55ea1933e0d6188a5b5c50bf354346500025
Itallo Arthur <itallo2002@hotmail.com>
2026-01-21 14:34:59 -0300

    Laços de Repetição em Js

---
+++ Aula13/ambiente.js
@@ -0,0 +1,12 @@
+var c = 1
+// while(c<=5){
+//     console.log(`passo ${c}`)
+//     c++ 
+// }
+
+do{
+    console.log(`passo ${c}`)
+    c++ 
+}while(c<=5)
+
+
---
+++ Aula14/ambiente.js
@@ -0,0 +1,6 @@
+//estrutura de repetição com variável de controle
+console.log('começando')
+for(var c=1 ; c <=10; c++){
+    console.log(c)
+}
+console.log('final')
