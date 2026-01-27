1f013f6289cfc6139981bf4da76a161f3b980a8f
Itallo Arthur <itallo2002@hotmail.com>
2026-01-27 13:27:37 -0300

    Exemplos e Exercícios de Funções

--- Aula15/ambiente.js
+++ Aula15/ambiente.js
@@ -1,9 +1,9 @@
 let num = [4,5,3,2,1]//fazerndo alguns experimentos com vetor 
-console.log(`nosso vetor é o ${num}`)
+console.log(`nosso vetor é o: ${num}`)
 num.push(53)
-console.log(`nosso vetor é o ${num}`)
-console.log(`nosso vetor é o ${num.length}`)
-console.log(`nosso vetor é o ${num.sort()}`)
+console.log(`nosso vetor agora é: ${num}`)
+console.log(`nosso vetor tem o total de: ${num.length} posições`)
+console.log(`nosso vetor de forma crescente agora é: ${num.sort()}`)
 
 for(var pos = 0; pos < num.length; pos++){
     console.log(num[pos])//utilizando o for para mostrar todos os elementos de um vetor
---
+++ Aula16/funcao1.js
@@ -0,0 +1,8 @@
+function pareimpar(n){
+    if(n %2 == 0){
+        return 'Par'
+    }else{
+        return 'Ímpar'
+    }
+}
+console.log(pareimpar(345))
